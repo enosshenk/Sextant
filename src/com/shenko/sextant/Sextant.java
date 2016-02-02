0fa98d8aa5bf011017491b2429d477ba5260990d
Matt <mattmolchan@gmail.com>
2016-02-02 03:53:55 -0500

    test

--- src/com/shenko/sextant/Sextant.java
+++ src/com/shenko/sextant/Sextant.java
@@ -6,14 +6,17 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Sextant {
 
-	public static LogFileHandler Handler;
+	//public static LogFileHandler Handler;
+	public static SQLHandler mySql;
 	
 	public static void main(String [] args) {
-		
-		Handler = new LogFileHandler();
-		
+		System.out.println("Started...");
+		//Handler = new LogFileHandler();
+		mySql = new SQLHandler();
+		mySql.connect();
+		//mySql.connect();
 		// Get latest log
-		System.out.println(Handler.GetLatestLogFile());	
+		//System.out.println(Handler.GetLatestLogFile());	
 		
 	}	
 
