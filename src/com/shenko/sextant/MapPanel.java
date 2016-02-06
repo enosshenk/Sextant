f6b7669bd3298b6da92e23aa993355c7be0dc2a7
Enos Shenk <enosshenk@comcast.net>
2016-02-06 09:34:17 -0500

    Restored lost MapPanel changes

--- src/com/shenko/sextant/MapPanel.java
+++ src/com/shenko/sextant/MapPanel.java
@@ -1,16 +1,26 @@
 package com.shenko.sextant;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -33,20 +43,26 @@ public class MapPanel extends JPanel {
     
     private GridPoint PlayerLocation;
     
-    private ImageIcon ShipIcon;
+    private ImageIcon ShipIcon, PortIcon;
     
     private boolean StartUp = true;
     private boolean Panning = false;
+    
+    private javax.swing.Timer Timer;
+    
+    private List<Port> Ports;
 
     public MapPanel(BufferedImage img) {
         mapX = 0;
         mapY = 0;
         this.img = img;
         PlayerLocation = new GridPoint();
-        
-        URL IconURL = getClass().getResource("/shipicon.png");
-        ShipIcon = new ImageIcon(IconURL);
 
+        ShipIcon = new ImageIcon(getClass().getResource("/shipicon.png"));
+        PortIcon = new ImageIcon(getClass().getResource("/porticon.png"));
+        
+        Ports = new CopyOnWriteArrayList<Port>();
+        
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent me) {
@@ -99,12 +115,27 @@ public class MapPanel extends JPanel {
               //  repaint();
             }           
             
-        });
+        }); 
+        
+        LoadPorts();
+        
+        // Set up and start timer to check mouseover
+        ActionListener Task = new ActionListener()
+        {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				repaint();
+				CheckPortsForMouseover();
+			}        	
+        };
+        
+        Timer = new javax.swing.Timer(20, Task);
+        Timer.start();
     }
 
     @Override
     protected void paintComponent(Graphics grphcs) {
- //       super.paintComponent(grphcs);
         Graphics2D g2d = (Graphics2D) grphcs;
         Component C = null;
         
@@ -117,8 +148,75 @@ public class MapPanel extends JPanel {
         // Draw map image
         g2d.drawImage(img, mapX, mapY, null);
         
+        // Draw ports
+        RenderPorts(grphcs);
+        
         // Draw player location icon
         ShipIcon.paintIcon(C, grphcs, Ship.X - 16 + mapX, Ship.Y - 16 + mapY);
+    }
+    
+    public void CheckPortsForMouseover()
+    {
+    	Point MousePos = getMousePosition();
+    	
+    	if (MousePos != null)
+    	{
+	    	for (Port p : Ports)
+	    	{
+				 GridPoint PortLoc = CoordinateToPixel( p.x, p.y );
+				 PortLoc.X = PortLoc.X + mapX;
+				 PortLoc.Y = PortLoc.Y + mapY;
+				 
+				 if ( (MousePos.x > PortLoc.X - 8 && MousePos.x < PortLoc.X + 8) 
+						 && (MousePos.y > PortLoc.Y - 8 && MousePos.y < PortLoc.Y) )
+				 {
+					 // Mouse cursor is over this port yay
+					 TextLayout PortText = GetTextForPort(p, this.getGraphics());
+					 PortText.draw((Graphics2D) this.getGraphics(), PortLoc.X + 16, PortLoc.Y + 16);
+				 }
+	    	}
+    	}
+    }
+    
+    public void RenderPorts(Graphics g)
+    {		 
+		 // Iterate ports and render icon at their locations
+		 for (Port p : Ports)
+		 {
+			 GridPoint PortLoc = CoordinateToPixel( p.x, p.y );
+			 PortLoc.X = PortLoc.X - 8 + mapX;
+			 PortLoc.Y = PortLoc.Y - 8 + mapY;
+			 PortIcon.paintIcon(this, g, PortLoc.X, PortLoc.Y);
+		 }
+    }
+    
+    public void LoadPorts()
+    {
+		// Iterate through sextant.portshash and set up an arraylist
+    	Iterator<Integer> portIterator = sextant.portsHash.keySet().iterator();
+		 while(portIterator.hasNext())
+		 {
+			 Integer key = portIterator.next();
+			 Port Port = sextant.portsHash.get(key);
+			 
+			 GridPoint PortLoc = CoordinateToPixel( Port.x, Port.y );
+			 PortLoc.X = PortLoc.X - 8 + mapX;
+			 PortLoc.Y = PortLoc.Y - 8 + mapY;
+			 Ports.add(Port);
+		 }
+    }
+    
+    public TextLayout GetTextForPort(Port p, Graphics g)
+    {
+    	// Constructs the string to be displayed when mousing over the given port
+    	Font font = new Font("TimesRoman", Font.PLAIN, 18);
+    	FontRenderContext frc = ((Graphics2D) (g)).getFontRenderContext();
+    	String PortString = p.name + System.getProperty("line.separator") + p.ID;
+    	
+    	TextLayout Layout = new TextLayout(PortString, font, frc);
+    	
+    	return Layout;
+    	
     }
 
     @Override
@@ -134,7 +232,7 @@ public class MapPanel extends JPanel {
     
     public void SetPlayerLocation(int inX, int inZ)
     {
-    	System.out.println("Set player loc in mappanel "+inX+" "+inZ);
+    	// Sets the player location and centers the map
     	PlayerLocation.SetLoc(inX, inZ);
     	
     	if (StartUp)
@@ -181,5 +279,22 @@ public class MapPanel extends JPanel {
     	repaint();
     }
     
+    public boolean CoordinateIsOnScreen(int inX, int inZ)
+    {
+    	// Returns true if the given game coordinates are viewable on the map pane givem
+    	// image panning
+    	GridPoint Pixel = CoordinateToPixel(inX, inZ);
+    	
+    	if ( (Pixel.X > mapX - 200 && Pixel.X < mapX + 200) && (Pixel.Y > mapY - 200 && Pixel.Y < mapY + 200) )
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    }
+    
+
 
 }
