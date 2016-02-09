c8d309ae1be3a11553b23dcf8a750ebe0e76fd6e
Matt <mattmolchan@gmail.com>
2016-02-08 23:24:00 -0500

    added scrollbars on map. now, just need a full size map image.

--- src/com/shenko/sextant/Window.java
+++ src/com/shenko/sextant/Window.java
@@ -188,11 +188,13 @@ public class Window extends JFrame {
 		}
 		MapPanel_1 = new MapPanel( Image, this );
 		MapPanel_1.setBackground(new Color(0, 255, 255));
-		MapPanel_1.setMinimumSize(new Dimension(400, 400));
-		MapPanel_1.setPreferredSize(new Dimension(400, 400));
-		MapPanel_1.setMaximumSize(new Dimension(500, 500));	
-		MapPanel_1.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.add(MapPanel_1);
+		MapPanel_1.setMinimumSize(new Dimension(4000, 4000));
+		MapPanel_1.setPreferredSize(new Dimension(4000, 4000));
+		MapPanel_1.setMaximumSize(new Dimension(5000, 5000));	
+		//MapPanel_1.setBorder(new EmptyBorder(5, 5, 5, 5));
+		JScrollPane v= new JScrollPane(MapPanel_1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        add(v);
+		//contentPane.add(MapPanel_1);
 		
 		MapTooltip = new MapTooltip();
 		MapTooltip.setMinimumSize(new Dimension(50, 50));
